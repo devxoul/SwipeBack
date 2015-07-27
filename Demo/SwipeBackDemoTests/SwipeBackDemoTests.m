613c34ad75b45ff874145a3c36f3a1ee6192f63e
Suyeol Jeon <devxoul@gmail.com>
2015-07-27 23:44:36 +0900

    Add a basic demo project.

---
+++ Demo/SwipeBackDemoTests/SwipeBackDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  SwipeBackDemoTests.m
+//  SwipeBackDemoTests
+//
+//  Created by 전수열 on 9/17/14.
+//  Copyright (c) 2014 Suyeol Jeon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SwipeBackDemoTests : XCTestCase
+
+@end
+
+@implementation SwipeBackDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
