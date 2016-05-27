842f41be00394072af6c77001d9faeffd56412f1
KimDarren <korean.darren@gmail.com>
2016-05-27 17:02:38 +0900

    Generate SwipeBackDefaults class

---
+++ SwipeBack/SwipeBackDefaults.h
@@ -0,0 +1,13 @@
+//
+//  SwipeBackDefaults.h
+//  SwipeBackDemo
+//
+//  Created by KimTae jun on 2016. 5. 27..
+//  Copyright © 2016년 Suyeol Jeon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SwipeBackDefaults : NSObject
+
+@end
---
+++ SwipeBack/SwipeBackDefaults.m
@@ -0,0 +1,13 @@
+//
+//  SwipeBackDefaults.m
+//  SwipeBackDemo
+//
+//  Created by KimTae jun on 2016. 5. 27..
+//  Copyright © 2016년 Suyeol Jeon. All rights reserved.
+//
+
+#import "SwipeBackDefaults.h"
+
+@implementation SwipeBackDefaults
+
+@end
